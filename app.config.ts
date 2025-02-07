42695a1e63c7fe109d57d4d10637f539a1d7f22f
Harenabs21 <hei.harena.6@gmail.com>
2025-02-07 09:27:13 +0300

    add expo property and rebuild

--- app.config.ts
+++ app.config.ts
@@ -1,52 +1,54 @@
 import 'dotenv/config';
 
-export default ({ config }: any) => ({
-    ...config,
-    name: 'firebase-notification-test',
-    slug: 'firebase-notification-test',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/images/icon.png',
-    scheme: 'myapp',
-    userInterfaceStyle: 'automatic',
-    newArchEnabled: true,
-    ios: {
-        supportsTablet: true,
-    },
-    android: {
-        adaptiveIcon: {
-            foregroundImage: './assets/images/adaptive-icon.png',
-                backgroundColor: '#ffffff',
-        },
-        package: process.env.ANDROID_PACKAGE_NAME,
-        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-    },
-    web: {
-        bundler: 'metro',
-        output: 'static',
-        favicon: './assets/images/favicon.png',
-    },
-    plugins: [
-        'expo-router',
-        [
-            'expo-splash-screen',
-            {
-                image: './assets/images/splash-icon.png',
-                imageWidth: 200,
-                resizeMode: 'contain',
-                backgroundColor: '#ffffff',
+export default {
+
+    expo: {
+            name: 'firebase-notification-test',
+            slug: 'firebase-notification-test',
+            version: '1.0.0',
+            orientation: 'portrait',
+            icon: './assets/images/icon.png',
+            scheme: 'myapp',
+            userInterfaceStyle: 'automatic',
+            newArchEnabled: true,
+            ios: {
+                supportsTablet: true,
             },
-        ],
-    ],
-    experiments: {
-        typedRoutes: true,
+            android: {
+                adaptiveIcon: {
+                    foregroundImage: './assets/images/adaptive-icon.png',
+                        backgroundColor: '#ffffff',
+                },
+                package: process.env.ANDROID_PACKAGE_NAME,
+                googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+            },
+            web: {
+                bundler: 'metro',
+                output: 'static',
+                favicon: './assets/images/favicon.png',
+            },
+            plugins: [
+                'expo-router',
+                [
+                    'expo-splash-screen',
+                    {
+                        image: './assets/images/splash-icon.png',
+                        imageWidth: 200,
+                        resizeMode: 'contain',
+                        backgroundColor: '#ffffff',
+                    },
+                ],
+            ],
+            experiments: {
+                typedRoutes: true,
+            },
+            extra: {
+                router: {
+                    origin: false,
+                },
+                eas: {
+                    projectId: '182dadfe-f0e6-4317-a87c-c559980a7932',
+                },
+            },
     },
-    extra: {
-        router: {
-            origin: false,
-        },
-        eas: {
-            projectId: '182dadfe-f0e6-4317-a87c-c559980a7932',
-        },
-    },
-});
+};
